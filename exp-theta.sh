0493dadf13e972bea5614c86f270ad59547335c1
gaoxy914 <gaoxy@hit.edu.cn>
2024-07-19 17:08:50 +0800

    exp

--- exp-theta.sh
+++ exp-theta.sh
@@ -2,9 +2,55 @@
 
 for data in ind/ anti/ corr/
 do
-    ./main PDBB $data 10000 6 0.5 6 >> res.txt
+    ./main PDBB $data 10000 6 0.5 6 0.01 >> res.txt
     for theta in 10 20 40 80 160 320 640 1280
     do
         ./main PRE $data 10000 6 0.5 6 $theta >> res.txt
+    done
+done
+
+for data in ind/ anti/ corr/
+do
+    for vare in 0.1 0.01 0.001 0.0001 0.00001
+    do
+        ./main GDY $data 10000 6 0.5 6 $vae >> res.txt
+    done
+done
+
+echo "n = 100000" >> res.txt
+
+for data in ind/ anti/ corr/
+do
+    ./main PDBB $data 100000 6 0.5 6 0.01 >> res.txt
+    for theta in 10 20 40 80 160 320 640 1280
+    do
+        ./main PRE $data 100000 6 0.5 6 $theta >> res.txt
+    done
+done
+
+for data in ind/ anti/ corr/
+do
+    for vare in 0.1 0.01 0.001 0.0001 0.00001
+    do
+        ./main GDY $data 100000 6 0.5 6 $vae >> res.txt
+    done
+done
+
+echo "l = 10" >> res.txt
+
+for data in ind/ anti/ corr/
+do
+    ./main PDBB $data 100000 6 0.5 10 0.01 >> res.txt
+    for theta in 10 20 40 80 160 320 640 1280
+    do
+        ./main PRE $data 100000 6 0.5 10 $theta >> res.txt
+    done
+done
+
+for data in ind/ anti/ corr/
+do
+    for vare in 0.1 0.01 0.001 0.0001 0.00001
+    do
+        ./main GDY $data 100000 6 0.5 10 $vae >> res.txt
     done
 done
