7e5963d7694984ea463062aa9377647576945360
gaoxy914 <gaoxy@hit.edu.cn>
2024-07-22 16:29:40 +0800

    exp

--- exp-2d.sh
+++ exp-2d.sh
@@ -8,24 +8,27 @@ do
         timeout 1h ./main DP $data $n 2 0.5 6 >> res.txt
         timeout 1h ./main DP+ $data $n 2 0.5 6 >> res.txt
         timeout 1h ./main GDY $data $n 2 0.5 6 0.01 >> res.txt
-        timeout 1h ./main PDBB $data $n 2 0.5 6 0.01 >> res.txt
+        timeout 1h ./main PDBB $data $n 2 0.5 6 >> res.txt
+        timeout 1h ./main PRE $data $n 2 0.5 6 160 >> res.txt
     done
 
     for p in 0.2 0.35 0.5 0.65 0.8
     do
         timeout 1h ./main BSL $data 10000 2 $p 6 >> res.txt
-        ./main DP $data 10000 2 $p 6 >> res.txt
-        ./main DP+ $data 10000 2 $p 6 >> res.txt
-        ./main GDY $data 10000 2 $p 6 0.01 >> res.txt
-        ./main PDBB $data 10000 2 $p 6 0.01 >> res.txt
+        timeout 1h ./main DP $data 10000 2 $p 6 >> res.txt
+        timeout 1h ./main DP+ $data 10000 2 $p 6 >> res.txt
+        timeout 1h ./main GDY $data 10000 2 $p 6 0.01 >> res.txt
+        timeout 1h ./main PDBB $data 10000 2 $p 6 >> res.txt
+        timeout 1h ./main PRE $data 10000 2 $p 6 160 >> res.txt
     done
 
-    for l in 2 4 6 8 10 12 14 16 18 20
+    for l in 2 4 6 8 10
     do
         timeout 1h ./main BSL $data 10000 2 0.5 $l >> res.txt
         timeout 1h ./main DP $data 10000 2 0.5 $l >> res.txt
         timeout 1h ./main DP+ $data 10000 2 0.5 $l >> res.txt
         timeout 1h ./main GDY $data 10000 2 0.5 $l 0.01 >> res.txt
-        timeout 1h ./main PDBB $data 10000 2 0.5 $l 0.01 >> res.txt
+        timeout 1h ./main PDBB $data 10000 2 0.5 $l >> res.txt
+        timeout 1h ./main PRE $data 10000 2 0.5 $l 160 >> res.txt
     done
 done
